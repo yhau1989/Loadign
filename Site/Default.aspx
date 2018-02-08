3d904ac787ac0acdfa13d0b02bb6eddb9d81934e
Samuel Pilay Muñoz <yhau1989@gmail.com>
2018-02-08 02:03:31 -0500

    Add project files.

---
+++ Site/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Site.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    
+    <div class="ui center aligned  segment container">
+        <h1 class="ui header">Default.aspx</h1>
+        <div class="divided"></div>
+        <div>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                <ContentTemplate>
+                    <asp:Button ID="Button1" OnClick="Button1_Click" CssClass="positive ui button" runat="server" Text="Load Json" />
+                </ContentTemplate>
+            </asp:UpdatePanel>
+        </div>
+        <div class="divided"></div>
+
+
+        <div class="ui right aligned container basic segment container">
+          <p>
+              <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+                <ContentTemplate>
+                    <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+                </ContentTemplate>
+            </asp:UpdatePanel>
+          </p>
+        </div>
+
+       
+             
+    
+    </div>
+    
+
+</asp:Content>
---
+++ Site/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Site
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebClient cliente = new WebClient();
+            string json = cliente.DownloadString("https://www.metaweather.com/api/location/search/?query=london");
+
+            dynamic stuff = JsonConvert.DeserializeObject(json);
+
+           
+
+            Label1.Text += @"<div class='ui divided selection list'>";
+            Label1.Text += @"<a class='item'><div class='ui black horizontal label'>Title</div> "+ stuff[0].title  + "</a>";
+            Label1.Text += @"<a class='item'><div class='ui black horizontal label'>Location Type</div> "+ stuff[0].location_type  + "</a>";
+            Label1.Text += @"<a class='item'><div class='ui black horizontal label'>Woeid</div> "+ stuff[0].woeid  + "</a>";
+            Label1.Text += @"<a class='item'><div class='ui black horizontal label'>Latt Long</div> "+ stuff[0].latt_long  + "</a>";
+            Label1.Text +=  "</div>";
+
+        }
+    }
+}
